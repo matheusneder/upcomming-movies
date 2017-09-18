c73a077588b35e21a68c2a32dedfe2a8480a3229
Matheus Neder <matheusneder@gmail.com>
2017-09-18 15:28:02 -0300

    Update CoreException.cs

--- Source/UpcommingMovies.Core.Domain/Exceptions/CoreException.cs
+++ Source/UpcommingMovies.Core.Domain/Exceptions/CoreException.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace UpcommingMovies.Core.Domain.Exceptions
 {
@@ -6,6 +6,7 @@ namespace UpcommingMovies.Core.Domain.Exceptions
     /// Base exception class for core expected errors. 
     /// This kind of error is the ones which will be handle at UI layer in order to friendly inform 
     /// the user about what happened.
+    /// Messages for this kind of exception must be focus the end user and must be internationalized.
     /// </summary>
     public class CoreException : Exception
     {
